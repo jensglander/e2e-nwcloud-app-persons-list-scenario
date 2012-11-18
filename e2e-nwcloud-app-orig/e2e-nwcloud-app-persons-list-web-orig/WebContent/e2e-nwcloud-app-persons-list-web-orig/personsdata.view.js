0c1f712d8beec0682a801a9fab6942fdd99d0452
jensglander <jens.glander@gmail.com>
2012-11-18 06:28:44 +0100

    Section 2.1: Web Project Creation
    - Section 2.1 "Create e2e-nwcloud-app-persons-list-web Project for
      SAPUI5 Application Development" steps were executed

---
+++ e2e-nwcloud-app-orig/e2e-nwcloud-app-persons-list-web-orig/WebContent/e2e-nwcloud-app-persons-list-web-orig/personsdata.view.js
@@ -0,0 +1,11 @@
+sap.ui.jsview("e2e-nwcloud-app-persons-list-web-orig.personsdata", {
+
+      getControllerName : function() {
+         return "e2e-nwcloud-app-persons-list-web-orig.personsdata";
+      },
+
+      createContent : function(oController) {
+
+      }
+
+});
