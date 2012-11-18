8e41a535fa01cef9d89ce65b2cd6a9a339e64b78
jensglander <jens.glander@gmail.com>
2012-11-18 06:31:48 +0100

    Section 2.2: Add a SAPUI5 button to Application View
    - Steps of section 2.2 were executed

--- e2e-nwcloud-app-orig/e2e-nwcloud-app-persons-list-web-orig/WebContent/e2e-nwcloud-app-persons-list-web-orig/personsdata.view.js
+++ e2e-nwcloud-app-orig/e2e-nwcloud-app-persons-list-web-orig/WebContent/e2e-nwcloud-app-persons-list-web-orig/personsdata.view.js
@@ -5,7 +5,16 @@ sap.ui.jsview("e2e-nwcloud-app-persons-list-web-orig.personsdata", {
       },
 
       createContent : function(oController) {
+    	  // create the button instance
+    	  var myButton = new sap.ui.commons.Button("btn");
 
+    	  // set properties, e.g. the text (there is also a shorter way of setting several properties)
+    	  myButton.setText("Hello World!");
+
+    	  // attach an action to the button's "press" event (use jQuery to fade out the button)
+    	  myButton.attachPress(function(){$("#btn").fadeOut();});
+
+    	  return myButton;
       }
 
 });
