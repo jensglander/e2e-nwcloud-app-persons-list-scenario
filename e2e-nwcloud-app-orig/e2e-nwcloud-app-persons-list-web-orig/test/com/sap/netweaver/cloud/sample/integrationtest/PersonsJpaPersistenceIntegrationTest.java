cdf7a62b47d519c89a8bc3cdb561f48c26e27622
jensglander <jens.glander@gmail.com>
2012-11-18 06:41:45 +0100

    Appendix A2-1: Automated Integration Testing (Servlets)
    - Steps of Appendix A2-1 were executed

---
+++ e2e-nwcloud-app-orig/e2e-nwcloud-app-persons-list-web-orig/test/com/sap/netweaver/cloud/sample/integrationtest/PersonsJpaPersistenceIntegrationTest.java
@@ -0,0 +1,109 @@
+package com.sap.netweaver.cloud.sample.integrationtest;
+
+import static org.hamcrest.Matchers.containsString;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebForm;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+
+/**
+ * Integration test using HttpUnit.
+ */
+public class PersonsJpaPersistenceIntegrationTest {
+    private static String serverUrl;
+    private static String nameSupplement;
+    private static String webModuleContextRoot;
+    private static String servletName;
+
+    private WebConversation wc;
+    private WebRequest request;
+    private WebResponse response;
+
+    /**
+     * Take a provided server URL usually fed in from outside through the build process or default to the local server
+     * as provided through Eclipse to run the integration test against.
+     */
+    @BeforeClass
+    public static void setupSuite() {
+        webModuleContextRoot = "e2e-nwcloud-app-persons-list-web-orig";
+        servletName = "PersonsJpaPersistenceServlet";
+
+    	// Get integration test server URL
+        serverUrl = System.getProperty("integration.test.server.url");
+        if (serverUrl == null) {
+            serverUrl = "http://localhost:8080";
+        }
+        System.out.println("Running against " + serverUrl);
+
+        // Compute random name supplement that is appended and checked for later during the tests
+        nameSupplement = Long.toHexString(new Random(System.currentTimeMillis()).nextLong());
+    }
+
+    /**
+     * Prepare a new web conversation for each test (grouping the requests/responses exchanged with the server).
+     */
+    @Before
+    public void setupTest() {
+        // Open web conversation
+        wc = new WebConversation();
+    }
+
+    /**
+     * Call the main servletName and check that it contains the expected content.
+     */
+    @Test
+    public void testPersistenceWithJPAServlet() throws IOException, SAXException {
+        // Call servlet
+        request = new GetMethodWebRequest(serverUrl + "/" + webModuleContextRoot + "/" + servletName);
+        response = wc.getResponse(request);
+        Assert.assertThat(response.getText(), containsString("Servlet for Adding and Displaying persisted Person Entities (with JPA)"));
+    }
+
+    /**
+     * Try to add one person and check that only this person is found.
+     */
+    @Test
+    public void testAddingFirstPerson() throws IOException, SAXException {
+        // Call servlet
+        request = new GetMethodWebRequest(serverUrl + "/" + webModuleContextRoot + "/" + servletName);
+        response = wc.getResponse(request);
+
+        // Fill out form and submit a person
+        WebForm form = response.getForms()[0];
+        form.setParameter("FirstName", "John_" + nameSupplement);
+        form.setParameter("LastName", "Doe_" + nameSupplement);
+        response = form.submit();
+        Assert.assertThat(response.getText(), containsString("John_" + nameSupplement));
+        Assert.assertThat(response.getText(), containsString("Doe_" + nameSupplement));
+    }
+
+    /**
+     * Try to add another person and check that this and the former person are found.
+     */
+    @Test
+    public void testAddingSecondPerson() throws IOException, SAXException {
+        // Call servlet
+        request = new GetMethodWebRequest(serverUrl + "/" + webModuleContextRoot + "/" + servletName);
+        response = wc.getResponse(request);
+
+        // Fill out form and submit another person
+        WebForm form = response.getForms()[0];
+        form.setParameter("FirstName", "Jane_" + nameSupplement);
+        form.setParameter("LastName", "Doe_" + nameSupplement);
+        response = form.submit();
+        Assert.assertThat(response.getText(), containsString("John_" + nameSupplement));
+        Assert.assertThat(response.getText(), containsString("Jane_" + nameSupplement));
+        Assert.assertThat(response.getText(), containsString("Doe_" + nameSupplement));
+    }
+}
